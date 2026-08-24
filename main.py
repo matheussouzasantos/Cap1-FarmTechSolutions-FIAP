8c79cdd08e08136d0428c66ec275543123140c6f
Matheus Souza Santos <contact@matheuscode.com>
2026-08-23 23:38:33 -0300

    Melhorando a navegacao dentro do Menu!

--- main.py
+++ main.py
@@ -29,11 +29,12 @@ while True:
     match opcao_menu:
         case 1:
             print("\n---------- CADASTRAR PLANTIO ----------")
-            nome_plantio = input("Nome do plantio: ")
+            nome_plantio = input("Digite o nome do plantio: ")
 
             print("\n1. Soja (área circular / pivô central)")
             print("2. Milho (área retangular / talhão)")
-            opcao_cultura = int(input("Opcao: "))
+            print("\n0. Voltar ao menu principal")
+            opcao_cultura = int(input("\nOpcao: "))
 
             # Matheus - Teste para ver se a opcao escolhida existe
             while opcao_cultura < 1 or opcao_cultura > 2:
@@ -51,10 +52,7 @@ while True:
             print("\n----------- CONSULTAR PLANTIOS -----------")
             # Matheus - Percorre todos os plantios e imprime no terminal
             for n, plantio in enumerate(plantios):
-                print(f"{n + 1}. {plantio}")
-                print(f"- Cultura: {culturas[n]}")
-                print(f"- Área: {areas[n]}m²")
-                #print(f"- Área: {areas[n]}") <- Falta cálculo de área para habilitar
+                print(f"{n + 1}. {plantio} | Cultura: {culturas[n]} | Área: {areas[n]}m²")
 
 
 
@@ -63,10 +61,15 @@ while True:
             print("Escolha o plantio que deseja atualizar:")
             # Matheus - Percorre todos os plantios e imprime no terminal
             for n, plantio in enumerate(plantios):
-                print(f"{n + 1}. {plantio}")
-                print(f"- Cultura: {culturas[n]}")
-                print(f"- Área: {areas[n]}m²")
-            opcao_plantio = int(input("Escolha uma opção: "))
+                print(f"{n + 1}. {plantio} | Cultura: {culturas[n]} | Área: {areas[n]}m²")
+
+            print("\n0. Voltar ao menu principal")
+            opcao_plantio = int(input("\nEscolha uma opção: "))
+
+            # Matheus - Teste para ver se a opcao escolhida existe
+            while opcao_plantio < 0 or opcao_plantio > len(plantios):
+                print("Opção inválida.")
+                opcao_plantio = int(input("Escolha uma opção: "))
 
 
 
@@ -75,14 +78,53 @@ while True:
             print("Escolha o plantio que deseja excluir:")
             # Matheus - Percorre todos os plantios e imprime no terminal
             for n, plantio in enumerate(plantios):
-                print(f"{n + 1}. {plantio}")
-                print(f"- Cultura: {culturas[n]}")
-                print(f"- Área: {areas[n]}m²")
-            opcao_plantio = int(input("Escolha uma opção: "))
+                print(f"{n + 1}. {plantio} | Cultura: {culturas[n]} | Área: {areas[n]}m²")
+
+            print("\n0. Voltar ao menu principal")
+            opcao_plantio = int(input("\nEscolha uma opção: "))
+
+            # Matheus - Teste para ver se a opcao escolhida existe
+            while opcao_plantio < 0 or opcao_plantio > len(plantios):
+                print("Opção inválida.")
+                opcao_plantio = int(input("Escolha uma opção: "))
+
+            if opcao_plantio != 0:
+                # Matheus - Remove o plantio escolhido e seus dados relacionados
+                plantios.pop(opcao_plantio - 1)
+                culturas.pop(opcao_plantio - 1)
+                comprimentos.pop(opcao_plantio - 1)
+                larguras.pop(opcao_plantio - 1)
+                raios.pop(opcao_plantio - 1)
+                areas.pop(opcao_plantio - 1)
+                insumos.pop(opcao_plantio - 1)
+                dosagens.pop(opcao_plantio - 1)
+
+                print("Plantio excluído com sucesso!")
+            else:
+                print("Voltando ao menu principal...")
 
 
+             
         case 5:
             print("\n----------- CALCULAR INSUMOS -----------")
+            print("Plations cadastrados:")
+            # Matheus - Percorre todos os plantios e imprime no terminal
+            for n, plantio in enumerate(plantios):
+                print(f"{n + 1}. {plantio} | Cultura: {culturas[n]} | Área: {areas[n]}m²")
+
+            print("\n0. Voltar ao menu principal")
+            opcao_plantio = int(input("\nEscolha uma opção: "))
+
+            # Matheus - Teste para ver se a opcao escolhida existe
+            while opcao_plantio < 0 or opcao_plantio > len(plantios):
+                print("Opção inválida.")
+                opcao_plantio = int(input("Escolha uma opção: "))
+
+            if opcao_plantio != 0:
+                # Aqui deve ser feito o Calculo de Insumos!
+                print("Calculo de insumos ainda precisa ser codado!")
+            else:
+                print("Voltando ao menu principal...")
 
 
 
